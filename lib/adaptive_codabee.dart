c6956827598b32acc4d85be873b0789291e9d0ef
Nufreort <ryryry203@gmail.com>
2022-03-01 12:13:38 +0100

    Check OS platform

--- lib/adaptive_codabee.dart
+++ lib/adaptive_codabee.dart
@@ -1,7 +1,11 @@
 library adaptive_codabee;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io';
+
+class Adaptive {
+
+  static bool isAndroid() => (Platform.isAndroid);
+
 }
